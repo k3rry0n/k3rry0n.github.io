b2a680e70ae12b5a4e6e1e259dae2c7e54b52c5b
Вячеслав Семикин < >
2016-06-20 14:22:21 +0300

    [CHABANY] Фикс положения текста

--- chabany/js/script.js
+++ chabany/js/script.js
@@ -1,3 +1,19 @@
+var input_field = document.getElementById('quote');
+var text = document.querySelector('.textabove');
+
+input_field.addEventListener('keyup', function(){
+			
+			if(text.innerHTML.length > 67 && text.innerHTML.length < 96) {
+				text.setAttribute('style', 'top: 30%');
+			};
+			if(text.innerHTML.length > 96){
+				text.setAttribute('style', 'top: 20%');
+			};
+			if (text.innerHTML.length > 118){
+				text.setAttribute('style', 'top: 20%; font-size: 36px;');
+			}		
+		});
+
 function generateImage(){ 
 	  
   html2canvas(document.querySelector(".preview"), {
@@ -13,6 +29,5 @@ function generateImage(){
 }
 
 function changeString() {
-	var text = document.querySelector('.textabove');
- 	text.innerHTML = document.querySelector('#quote').value;
+		text.innerHTML = document.querySelector('#quote').value;
 }
