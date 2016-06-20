75da200bc3346e95591192a547d967cf416eb208
Вячеслав Семикин < >
2016-06-20 14:16:52 +0300

    [LAMP] Фикс положения текста

--- lamp/js/script.js
+++ lamp/js/script.js
@@ -2,11 +2,15 @@ var input_field = document.getElementById('quote');
 var text = document.querySelector('.textabove');
 
 input_field.addEventListener('keyup', function(){
-			if(text.innerHTML.length > 67) {
+			
+			if(text.innerHTML.length > 67 && text.innerHTML.length < 96) {
 				text.setAttribute('style', 'top: 30%');
 			};
 			if(text.innerHTML.length > 96){
 				text.setAttribute('style', 'top: 20%');
+			};
+			if (text.innerHTML.length > 118){
+				text.setAttribute('style', 'top: 20%; font-size: 36px;');
 			}		
 		});
 
